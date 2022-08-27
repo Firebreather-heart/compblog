194df503b60ae7ef044e388e1da80f84b3d0fc1b
firebreather <dtenny95@gmail.com>
2022-08-27 10:58:58 +0100

    finetune

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,21 @@ from .forms import SearchForm
 from django.contrib.postgres.search import SearchVector,SearchQuery, SearchRank
 from django.db.models import Count
 # Create your views here.
+from django.contrib.syndication.views import Feed
+from django.template.defaultfilters import truncatewords
+from django.urls import reverse_lazy
+from .models import Post
 
+class LatestPostsFeed(Feed):
+    title = 'My blog'
+    link = reverse_lazy('home')
+    description = 'New posts of my blog.'
+    def items(self):
+        return Post.published.all()[:10]
+    def item_title(self, item):
+        return item.title
+    def item_description(self, item):
+        return truncatewords(item.body, 30)
 
 def post_list(request,tag_slug=None):
     if request.method == 'GET':
